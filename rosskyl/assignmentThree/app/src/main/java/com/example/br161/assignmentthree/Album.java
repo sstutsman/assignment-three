19f1db2e2d1d97fab4ba749802ec0d84e3dc7a51
rosskyl <rosskyl@mnstate.edu>
2015-03-30 17:17:53 -0500

    First draft of item_album.xml

--- rosskyl/assignmentThree/app/src/main/java/com/example/br161/assignmentthree/Album.java
+++ rosskyl/assignmentThree/app/src/main/java/com/example/br161/assignmentthree/Album.java
@@ -13,6 +13,8 @@ public class Album {
 
     private String genre;
 
+    private String albumCoverName;
+
     private String[] songs;
 
     private int numTracks;
@@ -20,43 +22,55 @@ public class Album {
     private int year;
 
 
-    public Album(String name, String artist, String publisher, String genre, String[] songs,
-                 int numTracks, int year) {
+    public Album(String name, String artist, String publisher, String genre, String albumCoverName,
+                 String[] songs, int numTracks, int year) {
 
         this.name = name;
         this.artist = artist;
         this.publisher = publisher;
         this.genre = genre;
+        this.albumCoverName = albumCoverName;
         this.songs = songs;
         this.numTracks = numTracks;
         this.year = year;
     }//end Album method
 
     public String getName() {
+
         return name;
     }//end getName method
 
     public String getArtist() {
+
         return artist;
     }//end getArtist method
 
     public String getPublisher() {
+
         return publisher;
     }//end getPublisher method
 
     public String getGenre() {
+
         return genre;
     }//end getGenre method
 
+    public String getAlbumCoverName() {
+        return albumCoverName;
+    }//end getAlbumCoverName
+
     public String[] getSongs() {
+
         return songs;
     }//end getSongs method
 
     public int getNumTracks() {
+
         return numTracks;
     }//end getNumTracks method
 
     public int getYear() {
+
         return year;
     }//end getYear method
 }//end Album class
