8b940caae42b84ffa64f3befbb1401e749c4f1da
Julio Salamanca <salamancju@mnstate.edu>
2015-04-01 18:19:43 -0500

    Changed layout for recyclerview and added empty activity view for spotify login

--- jcs4590/AlbumStore/app/src/main/java/com/example/julio/albumstore/AlbumAdapter.java
+++ jcs4590/AlbumStore/app/src/main/java/com/example/julio/albumstore/AlbumAdapter.java
@@ -1,13 +1,22 @@
 package com.example.julio.albumstore;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
+
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -17,6 +26,11 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
 
     private List<Album> albumList;
     protected LayoutInflater inflater;
+    private Bitmap bitmap;
+    private ImageView tempImageView;
+    private RecyclerView albumRecyclerView;
+
+
 
     public class AlbumViewHolder extends RecyclerView.ViewHolder {
 
@@ -30,6 +44,9 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
             nameView = (TextView) v.findViewById(R.id.nameTextView);
             artistView = (TextView) v.findViewById(R.id.artistNameTextView);
             imageView = (ImageView) v.findViewById(R.id.albumImageView);
+            albumRecyclerView = (RecyclerView) v.findViewById(R.id.albumRecycleView);
+
+
 
 
         }
@@ -40,6 +57,7 @@ public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumViewHol
 
         inflater = LayoutInflater.from(context);
         this.albumList = list;
+        tempImageView = new ImageView(context);
 
     }
 
@@ -70,11 +88,16 @@ return new AlbumViewHolder(view);
         Album album = albumList.get(i);
         albumViewHolder.nameView.setText(album.name);
         albumViewHolder.artistView.setText(album.artistName);
-        // albumViewHolder.imageView.
+
+
+        Picasso.with(albumViewHolder.imageView.getContext()).load(album.coverArtURLString).into(albumViewHolder.imageView);
 
 
 
     }
+
+
+
 
 
 
--- jcs4590/AlbumStore/app/src/main/java/com/example/julio/albumstore/MainActivity.java
+++ jcs4590/AlbumStore/app/src/main/java/com/example/julio/albumstore/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.julio.albumstore;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.*;
+
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
@@ -19,6 +20,9 @@ public class MainActivity extends Activity {
     private RecyclerView.Adapter albumReyclerViewAdpter;
     LinearLayoutManager layoutManager;
     private  List<Album> dataSet;
+    private static final String clientID = "6b7e14da031f4d72b6a3301c262f5647";
+    private static final String clientSecret = "a00298d93f164681b308aa3192909b19";
+
 
     private String[] albums;
 
@@ -33,28 +37,34 @@ public class MainActivity extends Activity {
 
 
         dataSet = new ArrayList<Album>();
-        dataSet.add(new Album("BLITZ","Julio Salamanca","http://www.faithmemorialchurch.org/wp-content/uploads/2013/12/Blitz.jpg"));
-        dataSet.add(new Album("CRAZYBITCH","Jennie Beldo","https://s-media-cache-ak0.pinimg.com/236x/2f/bf/6a/2fbf6ace6e9b4ccca5e09ac8d6c90055.jpg"));
+        dataSet.add(new Album("BLITZ","Ruffus","http://www.faithmemorialchurch.org/wp-content/uploads/2013/12/Blitz.jpg"));
+        dataSet.add(new Album("CRAZYBITCHES NEED LOVE TOO!","Killa Bitch","https://s-media-cache-ak0.pinimg.com/236x/2f/bf/6a/2fbf6ace6e9b4ccca5e09ac8d6c90055.jpg"));
 
 
         layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        albumsRecyclerView.setLayoutManager(layoutManager);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        albumsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
         albumReyclerViewAdpter = new AlbumAdapter(this,dataSet);
         albumsRecyclerView.setAdapter(albumReyclerViewAdpter);
-//        albumRecylerViewLayoutManger = new LinearLayoutManager(this);
-//        albumsRecyclerView.setLayoutManager(albumRecylerViewLayoutManger);
+
     }
 
 
     @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+
+    private void getRecentlyReleasedAlbums(){
+
+
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
