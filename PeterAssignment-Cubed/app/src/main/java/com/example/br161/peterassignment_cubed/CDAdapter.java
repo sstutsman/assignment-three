027cf09b9c250dd31a79464cf9aa56728766734b
unknown <rj6870zb@BR161-21.mnstate.edu>
2015-04-01 19:06:57 -0500

    Apparently didn't include some files to the previous commit.

--- PeterAssignment-Cubed/app/src/main/java/com/example/br161/peterassignment_cubed/CDAdapter.java
+++ PeterAssignment-Cubed/app/src/main/java/com/example/br161/peterassignment_cubed/CDAdapter.java
@@ -1,13 +1,14 @@
 package com.example.br161.peterassignment_cubed;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import java.util.List;
 
@@ -18,7 +19,7 @@ public class CDAdapter extends RecyclerView.Adapter<CDAdapter.ViewHolder> {
     List<CDAlbum> myCollection;
 
     //Constructor. Passes data from fragment
-    public CDAdapter(List<CDAlbum> myCollection){
+    public CDAdapter(List<CDAlbum> myCollection) {
         this.myCollection = myCollection;
     }
 
@@ -36,4 +37,61 @@ public class CDAdapter extends RecyclerView.Adapter<CDAdapter.ViewHolder> {
             }
         });
         return viewHolder;
+
     }
+
+    @Override
+    // This method assigns the values from the contacts list we passed in to the views
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.tvArtist.setText(myCollection.get(position).getAlbumArtist());
+        holder.tvAlbum.setText(myCollection.get(position).getAlbumName());
+        holder.imgAlbumCover.setImageResource(myCollection.get(position).getAlbumCover());
+    }
+
+    /*
+     *
+     * VISUAL BREAK OF CODE
+     *
+     *
+     */
+    public int getItemCount() {
+        return myCollection.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ItemClickListener listener;
+
+        ImageView imgAlbumCover;
+        TextView tvArtist;
+        TextView tvAlbum;
+
+
+        // We map our views, and assign listeners in the ViewHolder constructor
+        public ViewHolder(View itemView, ItemClickListener listener) {
+            super(itemView);
+            this.listener = listener;
+            imgAlbumCover = (ImageView) itemView.findViewById(R.id.draw_albumcover);
+            tvArtist = (TextView) itemView.findViewById(R.id.tv_artistname);
+            tvAlbum = (TextView) itemView.findViewById(R.id.tv_albumName);
+
+            tvArtist.setOnClickListener(this);
+            tvAlbum.setOnClickListener(this);
+        }
+
+        // This method is just to pass on the onClick event to our individual items! Neat!
+        @Override
+        public void onClick(View view) {
+            listener.onItemClick(view, getPosition());
+        }
+
+        // This is the interface which forces our Adapter to implement the OnClickListener
+        public interface ItemClickListener {
+            void onItemClick(View view, int position);
+        }
+    }
+}
+
+
+
