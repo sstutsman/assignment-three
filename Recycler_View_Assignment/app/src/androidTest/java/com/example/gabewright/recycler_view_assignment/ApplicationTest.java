7f5e79939f92eab00808b4ceb05b9bef9caf6319
gabeswright <gwright@cord.edu>
2015-04-06 11:44:41 -0500

    I screwed up the other assignment so I created a new project

---
+++ Recycler_View_Assignment/app/src/androidTest/java/com/example/gabewright/recycler_view_assignment/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.gabewright.recycler_view_assignment;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ Recycler_View_Assignment/app/src/main/java/com/example/gabewright/recycler_view_assignment/Album.java
@@ -0,0 +1,36 @@
+package com.example.gabewright.recycler_view_assignment;
+
+/**
+ * Created by GabeWright on 4/6/15.
+ */
+public class Album{
+    private String name;
+    private String artist;
+    private String tcount;
+    private String year;
+    private String publisher;
+
+    public Album(String name, String artist, String tcount,String year, String publisher){
+        this.name = name;
+        this.artist = artist;
+        this.tcount = tcount;
+        this.year = year;
+        this.publisher = publisher;
+
+    }
+
+    public String getName()  {
+        return name;
+    }
+
+    public String getArtist()  {
+        return artist;
+    }
+
+    public String getTcount() { return tcount; }
+
+    public String getYear() { return year; }
+
+    public String getPublisher() { return publisher; }
+
+}
