9162b1404cbe3b820f3f861e9e59d785845c3197
BigBearin <brandinbarren@hotmail.com>
2015-04-08 23:29:57 -0500

    Trying to add other files, it is not working...

---
+++ BigBearin/AlbumList/app/src/main/java/com/albumlist/albumlist/EscapeActivity.java
@@ -0,0 +1,14 @@
+package com.albumlist.albumlist;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EscapeActivity extends Activity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.escape_layout);
+    }
+
+}
